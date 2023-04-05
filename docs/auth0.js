04c7ffdb8cdb44a68b44bc49256076e447c420ff
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2023-04-05 15:58:56 -0300

    feat: add auth

---
+++ docs/auth0.js
@@ -0,0 +1,7 @@
+var auth0 = new auth0.WebAuth({
+    domain: 'dev-8mgq5i2i47f1wjgl.us.auth0.com',
+    clientID: 'wY2K4tJOj9tpgZmRddsQatuosdMTHu8t',
+    redirectUri: 'https://carloslopezsoto.github.io/DocsPrincipal/',
+    responseType: 'token id_token',
+    scope: 'openid'
+    });
